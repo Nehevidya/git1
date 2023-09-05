98a2416905d41922d42f0ae023223dc3a9ae092f
Nehevidya <hase.vidya@gmail.com>
2023-09-05 19:34:11 +0530

    day2

---
+++ script3.js
@@ -0,0 +1,14 @@
+// printing statement
+console.log("hi")
+// information store
+let x =100
+// keyword variblename = value
+console.log(x)
+// update
+ x=200
+ //console.log(x)
+ console.log(200)
+
+ const y =500
+ console.log(y)
+  // y=10
